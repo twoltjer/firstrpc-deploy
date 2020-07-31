44ed24a13a73ff6610d12ac595d16cb3e9c30217
Thomas Woltjer <thomas@thomaswoltjer.com>
2020-07-31 13:51:39 -0400

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@ set -e
 git submodule update --init --recursive
 pushd _site
 git checkout master
-git pull
 popd
 bundle exec jekyll build
 cp -R Media _site
+rm _site/README.md
+cp -R deploy-conf-files/* _site
 pushd _site
 git add -A
 git commit -m "Deploy script"
